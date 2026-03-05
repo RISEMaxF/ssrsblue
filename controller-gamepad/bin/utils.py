4169c0d4b40614b4e759b704cc02ec54550a4394
Max Fransson <max.fransson@ri.se>
2026-03-05 10:29:21 +0100

    Fix TimestampedBool import path to match keelson 0.5.0

--- controller-gamepad/bin/utils.py
+++ controller-gamepad/bin/utils.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
+import time
+
 from keelson import enclose  # noqa: F401
 from keelson.helpers import enclose_from_float, enclose_from_integer, enclose_from_string  # noqa: F401
+from keelson.payloads.Primitives_pb2 import TimestampedBool
+
+
+def enclose_from_boolean(value: bool, timestamp: int = None) -> bytes:
+    payload = TimestampedBool()
+    payload.timestamp.FromNanoseconds(timestamp or time.time_ns())
+    payload.value = value
+    return enclose(payload.SerializeToString())
